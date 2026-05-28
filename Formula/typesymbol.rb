22703d5eeff79bd8c551a186c11652dc5dae237d
yazanmwk <yazan.mw.k@gmail.com>
2026-05-29 01:21:32 +0400

    Update typesymbol to v0.1.19

--- Formula/typesymbol.rb
+++ Formula/typesymbol.rb
@@ -1,15 +1,15 @@
 class Typesymbol < Formula
   desc "System-wide math shorthand daemon"
   homepage "https://github.com/yazanmwk/TypeSymbol"
-  version "0.1.18"
+  version "0.1.19"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yazanmwk/TypeSymbol/releases/download/v0.1.18/typesymbol-v0.1.18-aarch64-apple-darwin.tar.gz"
-      sha256 "9eb6a9a4ff57cb6374042d6c6a4d22718e7b0dda567ab99e7e6969eaa451de66"
+      url "https://github.com/yazanmwk/TypeSymbol/releases/download/v0.1.19/typesymbol-v0.1.19-aarch64-apple-darwin.tar.gz"
+      sha256 "37b83dca6b0df43164501f5f180d86328feff31dd549a9b9bb5a0a66ba9ffaf3"
     else
-      url "https://github.com/yazanmwk/TypeSymbol/releases/download/v0.1.18/typesymbol-v0.1.18-x86_64-apple-darwin.tar.gz"
-      sha256 "b21897fd2391b84325196c5e752b565b8e5b260fd160f4bc26f365743cb47726"
+      url "https://github.com/yazanmwk/TypeSymbol/releases/download/v0.1.19/typesymbol-v0.1.19-x86_64-apple-darwin.tar.gz"
+      sha256 "46a39f40c40fc67b5a178a5ac46aaea46989da8f67ec023cb3d8bf287a064b92"
     end
   end
 
